f3f57de2f73c0e5b5e11e7f0489d72853b0ed135
sergio <davidjunyor@live.com>
2024-07-26 22:31:44 -0400

    1.4

--- Tipos_variaveis/loops.py
+++ Tipos_variaveis/loops.py
@@ -1,3 +1,36 @@
+print("for utilizando lista")
 lista = [1, 2, 3, 4, 5]
 for i in lista: 
   print(i)
+
+print("for utilizando tupla")
+tupla = (1, 2, 3, 4, 5)
+for j in tupla:
+  print(j)
+
+
+pessoa = {"nome":"joão", "idade":30, "cidade":"manaus"}
+print("for utilizando dicionário - chaves")
+
+for chave in pessoa.keys():
+
+  print(chave)
+
+print("for utilizando dicionário - valor")
+
+for valor in pessoa.values():
+  print(valor)
+
+for chave, valor in pessoa.items():
+  print(f"{chave}: {valor}")
+
+  # range(): intervalo numerico
+  #[0, 1,2,3,4,5,6,7,8,9]
+for numero in range(1,5):
+  print("numero: ", numero)
+
+#função range com len
+print("\nutilizando a função range com len")
+lista = [1, 2, 3, 4, 5]
+for indice in range(0,len(lista)):
+  print("indice:", indice)
