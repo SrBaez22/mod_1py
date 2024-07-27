153163f9f0688cd43b01919bdebfa18a52f0dbc1
sergio <davidjunyor@live.com>
2024-07-26 23:50:12 -0400

    estudos de python 1.5

---
+++ Tipos_variaveis/loop while.py
@@ -0,0 +1,7 @@
+contador = 0
+while contador < 5:
+  print("voce não pode acessar esse site", contador)
+  contador +=1
+  if contador == 4:
+    break
+  print("programa finalizado")
---
+++ Tipos_variaveis/projeto/gerenciador.py
@@ -0,0 +1,2 @@
+print("menu do gerenciador da lista de tarefas")
+opcoes = {"1.": "Adicionar tarefa"}
